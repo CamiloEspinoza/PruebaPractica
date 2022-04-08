c54571f583c1da3e0f50b3cfc8fd11b30b6dd686
Ricardo Valenzuela <rvalenzuelaferrada@gmail.com>
2022-04-08 14:03:18 -0400

    Add model dia with casts

---
+++ dailyphoto/app/Models/Dia.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dia extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'fecha' => 'date',
+        'estado' => 'boolean',
+        'imagenes' => 'array',
+    ];
+}
