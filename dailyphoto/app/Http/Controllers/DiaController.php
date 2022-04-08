8170fb574ee454fba10d6792501bc046674782aa
Ricardo Valenzuela <rvalenzuelaferrada@gmail.com>
2022-04-08 16:10:01 -0400

    Add day controller index and store

---
+++ dailyphoto/app/Http/Controllers/DiaController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDiaRequest;
+use App\Models\Dia;
+use Illuminate\Http\Request;
+
+class DiaController extends Controller
+{
+    /**
+     * Get days, optionally filtered by period.
+     *
+     * @return Dia
+     */
+    public function index(): Dia
+    {
+        return Dia::when(request('inicio'), function ($query) {
+            $query->where('fecha', '>=', request('inicio'));
+        })->when(request('termino'), function ($query) {
+            $query->where('fecha', '<=', request('termino'));
+        })->get();
+    }
+
+    /**
+     * Create days period.
+     *
+     * @param StoreDiaRequest $request
+     * @return void
+     */
+    public function store(StoreDiaRequest $request): void
+    {
+        (new Dia())->createPeriod($request->inicio, $request->termino);
+    }
+}
--- dailyphoto/app/Models/Dia.php
+++ dailyphoto/app/Models/Dia.php
@@ -2,12 +2,21 @@
 
 namespace App\Models;
 
+use Carbon\CarbonPeriod;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Http;
 
 class Dia extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['fecha', 'descripcion', 'estado', 'imagenes'];
 
     /**
      * The attributes that should be cast.
@@ -19,4 +28,36 @@ class Dia extends Model
         'estado' => 'boolean',
         'imagenes' => 'array',
     ];
+
+    /**
+     * Create a new period between two dates.
+     *
+     * @param string $inicio
+     * @param string $termino
+     * @return void
+     */
+    public function createPeriod(string $inicio, string $termino)
+    {
+        $period = CarbonPeriod::create($inicio, $termino);
+
+        foreach ($period as $date) {
+            $this->create([
+                'fecha' => $date,
+                'estado' => false,
+            ]);
+        }
+    }
+
+    public function getImageFromNasa()
+    {
+        $response = Http::get('https://api.nasa.gov/planetary/apod?date=' . $this->fecha->toDateString() . '&api_key=' . env('NASA_API_KEY'));
+
+        if ($response->successful()) {
+            $data = $response->json();
+
+            if (isset($data['url'])) {
+                return $data['url'];
+            }
+        }
+    }
 }
